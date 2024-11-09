a0a69722a433cbfb80ee997f98d999752254956f
Ruxvania <132949475+Ruxvania@users.noreply.github.com>
2024-11-09 13:52:47 -0500

    Update word bans and fix help tb command

--- common-functions.js
+++ common-functions.js
@@ -45,7 +45,7 @@ function isStringVaild(string) {
 function censor(message, omitOrReport, ...filters) {
     const filterRegex = {
         banned: /discord|sonic|fortnite|among us|corona|roblox/gi,
-        hardbanned: /exe|forkie|runkit\.sh|^\*hugs|(?<!^)chat/img,
+        hardbanned: /exe|forkie|runkit\.sh|^\*hugs|discord/img,
         link: /(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/gm,
         gayleb: /^\s*![a-zA-Z]*/gm,
         ping: /@everyone|@here|<@[0-9]+>/gi,
--- trollbox/commands/help.js
+++ trollbox/commands/help.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { __dirname } from '../../common-functions.js';
+import { censor } from '../../common-functions.js';
 import config from "../../config.json" with { type: "json" };
 
 export default {
@@ -9,9 +9,9 @@ export default {
   description: 'View all commands and info about the bot.',
   async execute(data, message, args) {
 
-    const helpHeader = fs.readFileSync(path.join(__dirname, "trollbox/commands/help-header.txt"), "utf8").trim();
+    const helpHeader = fs.readFileSync(path.join(import.meta.dirname, "help-header.txt"), "utf8").trim();
 
-    let helpFooter = fs.readFileSync(path.join(__dirname, "trollbox/commands/help-footer.txt"), "utf8").trim();
+    let helpFooter = fs.readFileSync(path.join(import.meta.dirname, "help-footer.txt"), "utf8").trim();
 
     helpFooter = config.version + "\n" + helpFooter;
 
@@ -21,6 +21,6 @@ export default {
       commands = commands.trim();
     };
 
-    data.tb.sendMessage(helpHeader + '\n\n' + commands + "\n\n" + helpFooter);
+    data.tb.sendMessage(censor(helpHeader + '\n\n' + commands + "\n\n" + helpFooter, "omit", "hardbanned"));
   },
 };
