5304cb9c6fd46e2080fb07eefd46ac9d158825fa
Ruxvania <132949475+Ruxvania@users.noreply.github.com>
2025-12-10 04:45:01 -0500

    remove gayleb censor from tb name command

--- trollbox/commands/name.js
+++ trollbox/commands/name.js
@@ -5,7 +5,7 @@ export default {
 	name: "name",
 	description: "Change the bot's name.",
 	async execute(data, message, args) {
-		if (isArgsVaild(args) == true && !censor(arrayToString(args), "report", "hardbanned", "gayleb")) {
+		if (isArgsVaild(args) == true && !censor(arrayToString(args), "report", "hardbanned")) {
             data.tb.name = arrayToString(args);
             data.tb.socket.emit("user joined", `${data.tb.genName()}`, data.tb.color, "", "");
         } else {
